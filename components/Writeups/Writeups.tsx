a7d98150f13a7bfccdbeb1f32033a36e94421ce2
Thamizhiniyan C S <csthamizhiniyan@gmail.com>
2024-06-27 17:24:17 +0530

    Updated nav links

--- components/Writeups/Writeups.tsx
+++ components/Writeups/Writeups.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <section
-      id="about"
+      id="writeups"
       className="h-[var(--section-height)] pt-[var(--section-padding-top)] w-full flex justify-start items-center flex-col"
     >
       <h1 className="py-5">Writeups</h1>
